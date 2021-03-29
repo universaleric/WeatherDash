2a673bcbbb27854f7634bad05df226bb7e16a184
Eric Hay <ericdanielhay@yahoo.com>
2021-03-29 16:42:23 -0500

    Search bar and button function correctly.

--- assets/script.js
+++ assets/script.js
@@ -25,43 +25,29 @@ let formSubmitHandler = function (event) {
     .then(response => response.json())
     .then(function(data){
         console.log(data);
-        displayRepos(data, city);
+        displayCities(data, city);
 
 })}
 
-let displayCities = function (cities, searchTerm) {
-    if (cities.length === 0) {
+let displayCities = function (data, city) {
+    if (data.length === 0) {
       cityContainerEl.textContent = 'No cities found.';
       return;
     }
   
-    citySearchTerm.textContent = searchTerm;
+    citySearchTerm.textContent = city;
   
-    for (let i = 0; i < cities.length; i++) {
-      let cityName = cities[i].owner.login + '/' + repos[i].name;
-  
-      var repoEl = document.createElement('a');
-      repoEl.classList = 'list-item flex-row justify-space-between align-center';
-      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-  
-      var titleEl = document.createElement('span');
-      titleEl.textContent = repoName;
-  
-      repoEl.appendChild(titleEl);
-  
-      var statusEl = document.createElement('span');
-      statusEl.classList = 'flex-row align-center';
-  
-      if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-      } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    for (let i = 0; i < data.length; i++) {
+      let forecast = {
+        date: data.list[i].dt_txt,
+        icon: data.list[i].weather[i].icon,
+        temp: "Temp: " + data.list[i].main.temp + "F" + "&#8451;",
+        humidity: "Humidity: " + data.list[i].main.humidity + "%"
       }
+
+      console.log(forecast);
+      cityContainerEl.textContent = forecast;
   
-      repoEl.appendChild(statusEl);
-  
-      repoContainerEl.appendChild(repoEl);
     }
   };
 
@@ -75,4 +61,4 @@ let displayCities = function (cities, searchTerm) {
 
 // });
 
-submitEl.addEventListener("submit", formSubmitHandler);
+submitEl.addEventListener("click", formSubmitHandler);
